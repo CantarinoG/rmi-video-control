b5c48b007dc9838a3d5481d1f71c9fe185c2362d
VS-7 <vitorsergio.ts@gmail.com>
2024-07-06 15:11:20 -0300

    Create GUI

--- data.py
+++ data.py
@@ -6,4 +6,44 @@ availableVideos = [
     {"name": "video3", "path": "./videos/v3.mp4"},
 ]
 
-ip = "192.168.3.109"
+video_data_list = [
+    {
+        "image": "./images/soccer-player-sport-character-eed4f9.webp",
+        "title": "História do Futebol 1",
+        "description": "Descrição do vídeo 1Descrição do vídeo 1Descrição do vídeo 1Descrição do vídeo 1Descrição do vídeo 1Descrição do vídeo 1"
+    },
+    {
+        "image": "https://example.com/image2.png",
+        "title": "História do Futebol 2",
+        "description": "Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2"
+    },
+    {
+        "image": "https://example.com/image2.png",
+        "title": "História do Futebol 2",
+        "description": "Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2Descrição do vídeo 2"
+    },
+
+]
+
+integrantes = [
+    {
+        "name": "Guilherme Cantarino",
+        "image": "./images/GuilhermeCantarino.png",
+        "github": "https://github.com/guilhermecantarino",
+        "matricula": "20221002030"
+    },
+    {
+        "name": "Vitor Sérgio",
+        "image": "./images/VitorSergio.png",
+        "github": "https://github.com/vitorsergio",
+        "matricula": "20221002031"
+    },
+    {
+        "name": "Thais Hipólito",
+        "image": "./images/Thais.png",
+        "github": "https://github.com/thais",
+        "matricula": "20221002032"
+    },
+]
+
+ip = "192.168.3.8"
