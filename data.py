85cb677a2326679664cbb9b86377f87cd4747cde
Thais Souza <75867936+so-tha@users.noreply.github.com>
2024-07-06 20:40:28 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 from video import Video
 
 availableVideos = [
-    {"name": "video1", "path": "./videos/v1.mp4"},
-    {"name": "video2", "path": "./videos/v2.mp4"},
-    {"name": "video3", "path": "./videos/v3.mp4"},
+    {"name": "video1", "path": "./videos/video1.mp4"},
+    {"name": "video2", "path": "./videos/video2.mp4"},
+    {"name": "video3", "path": "./videos/video3.mp4"},
 ]
 
 ip = "192.168.3.109"
