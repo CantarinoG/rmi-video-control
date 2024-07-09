847812da4db6b4f897b6153218d33bd518f4afb6
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-07-08 21:25:35 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,30 +1,29 @@
 from video import Video
 
 availableVideos = [
-    {"name": "video1", "path": "./videos/video2.mp4"},
-    {"name": "video2", "path": "./videos/video1.mp4"},
-    {"name": "video3", "path": "./videos/video3.mp4"},
+    {"name": "video 1", "path": "./videos/video1.mp4"},
+    {"name": "video 2", "path": "./videos/video2.mp4"},
+    {"name": "video 3", "path": "./videos/video3.mp4"},
 ]
 
 video_data_list = [
     {
-        "image": "./images/1.png",
+        "image": "./images/soccer-player-sport-character-eed4f9.webp",
         "title": "A Historia do Futebol",
         "description": "Neste vídeo abordamos o surgimento do Futebol, desde os seus primordios até a sua transformação no que estamos acostumados no dia de hoje."
     },
     {
-        "image": "./images/2.png",
+        "image": "./images/VAR.png",
         "title": "A Tecnologia Oculta do Futebol",
         "description": "Nesse vídeo é apresentado a tecnologia por trás das decisões do VAR."
     },
     {
-        "image": "https://example.com/image2.png",
+        "image": "./images/3.1.png",
         "title": "A Tecnologia da Linha do Gol",
         "description": "Nesse vídeo é apresentado soluções tecnológicas para monitorar os gols marcados em partidas da FIFA."
     },
 
 ]
-
 integrantes = [
     {
         "name": "Guilherme Cantarino",
@@ -52,4 +51,4 @@ integrantes = [
     },
 ]
 
-ip = "192.168.0.106"
+ip = "192.168.3.8"
